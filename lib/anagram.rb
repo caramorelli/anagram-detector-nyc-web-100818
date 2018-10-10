f384391903de1e7ce14ae7f48181dc32bf1f434a
Cara Morelli <caramorelli7@gmail.com>
2018-10-10 20:17:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,13 +3,12 @@
 class Anagram 
   attr_accessor :word
   
-  @@all_anagrams = []
-  
   def initialize(anagram)
     @anagram = anagram
   end 
   
   def match(array)
+    anagram_array = []
     array.each do |word|
       if word.length == @anagram.length && 
         word.chars.sort == @anagram.chars.sort 
